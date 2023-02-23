c51d1facd792f28b9de67dfa6ba3791bd8cdc713
Brandon Baker <bbaker.sh@gmail.com>
2023-02-22 20:07:21 -0800

    added styles

--- app/auth/nav.tsx
+++ app/auth/nav.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
 import Login from "./login";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
 
-
-export default async function Nav(){
-    return(
-        <nav className=" flex justify-between items-center py-8">
-            <Link href={"/"}>
-            <h1 className="font-bolt text-lg">Send It</h1>
-            </Link>
-            <ul className="flex items-center gap-6 ">
-                <Login />
-            </ul>
-        </nav>
-    )
+export default async function Nav() {
+  const session = await getServerSession(authOptions);
+  return (
+    <nav className=" flex justify-between items-center py-8 px-2">
+      <Link href={"/"}>
+        <h1 className="font-bolt text-lg">Send It</h1>
+      </Link>
+      <ul className="flex items-center gap-6 ">
+        {!session?.user && <Login/>}
+        {session?.user && <h1>{session.user.name}</h1>}
+      </ul>
+    </nav>
+  );
 }
