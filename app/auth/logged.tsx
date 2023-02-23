662195d4ec8b977c6f88c515d49077c199d5e9f6
Brandon Baker <bbaker.sh@gmail.com>
2023-02-22 20:34:33 -0800

    added img tage to show google user image

--- app/auth/logged.tsx
+++ app/auth/logged.tsx
@@ -2,11 +2,30 @@
 
 import Image from "next/image";
 import { signOut } from "next-auth/react";
+import Link from "next/link";
 
-export default function Logged(){
-    return(
-       <li className="flex gap-8 items-center">
-           <button className="text-sm bg-gray-600 text-white py-2 px-6 rounded-xl disabled:opacity-25">Sign Out</button> 
-       </li> 
-    )
-} 
+type User = {
+  image: string;
+};
+
+export default function Logged({ image }: User) {
+  return (
+    <li className="flex gap-8 items-center">
+      <button
+        onClick={() => signOut()}
+        className="text-sm bg-gray-600 text-white py-2 px-6 rounded-xl disabled:opacity-25"
+      >
+        Sign Out
+      </button>
+      <Link href={"/dashboard"}>
+        <Image
+          width={64}
+          height={64}
+          src={image}
+          alt="user-image"
+          className="w-14 rounded-full"
+        />
+      </Link>
+    </li>
+  );
+}
